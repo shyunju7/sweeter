e4af00f48885bb5f41b333606abdd813c2ab2031
shyunju7 <shyunju7@naver.com>
2021-11-15 18:58:24 +0900

    Feat Login form & input

--- sweeter/src/routes/Auth.js
+++ sweeter/src/routes/Auth.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [passowrd, setPassword] = useState("");
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") setEmail(value);
+    else if (name === "password") setPassword(value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          name="email"
+          type="text"
+          placeholder="E-Mail"
+          required
+          value={email}
+          onChange={onChange}
+          autoComplete="off"
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={passowrd}
+          onChange={onChange}
+          autoComplete="off"
+        />
+        <input type="submit" value="LOGIN" onClick={onSubmit} />
+      </form>
+
+      <div>
+        <button>Continue with Google</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
