5dcfee3124df2b3d913a73092c3ae3336b88a041
shyunju7 <shyunju7@naver.com>
2021-11-15 18:22:31 +0900

    Feat Router settings

---
+++ sweeter/src/components/Router.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+const AppRouter = ({ isLoggedIn }) => {
+  return (
+    <Router>
+      <Routes>
+        {isLoggedIn ? (
+          <Route exact path="/" element={<Home />} />
+        ) : (
+          <Route exact path="/" element={<Auth />} />
+        )}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
