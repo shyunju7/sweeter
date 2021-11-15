5472920991bb846a200de26aa53898ca8e89ff16
shyunju7 <shyunju7@naver.com>
2021-11-15 19:33:42 +0900

    Feat check CurrentUser

--- sweeter/src/components/App.js
+++ sweeter/src/components/App.js
@@ -1,13 +1,25 @@
 import AppRouter from "./Router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { authService } from "../fbBase";
 
 function App() {
-  console.log(authService.currentUser);
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
       <footer>&copy; {new Date().getFullYear()} Sweeter</footer>
     </>
   );
