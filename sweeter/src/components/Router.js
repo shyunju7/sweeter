30e8fec5273ba6c87b89a55c7699149b7356a736
shyunju7 <shyunju7@naver.com>
2021-11-16 16:14:55 +0900

    Feat Logout

--- sweeter/src/components/Router.js
+++ sweeter/src/components/Router.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Redirect,
+  Navigate,
+} from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route exact path="/" element={<Home />} />
+          <>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile" element={<Profile />} />
+          </>
         ) : (
           <Route exact path="/" element={<Auth />} />
         )}
