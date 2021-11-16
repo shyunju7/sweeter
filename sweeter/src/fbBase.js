8335072e7b0dd041e93359e1203692967c1ee107
shyunju7 <shyunju7@naver.com>
2021-11-16 14:21:58 +0900

    Config GoogleLoginProvider

--- sweeter/src/fbBase.js
+++ sweeter/src/fbBase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider } from "firebase/auth";
 
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 export const authService = getAuth();
+export const provider = new GoogleAuthProvider();
