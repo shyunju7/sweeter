7ac313b37ca28a6b086379cf246f9374a0274ab3
shyunju7 <shyunju7@naver.com>
2021-11-16 16:51:44 +0900

    Feat save sweet text

--- sweeter/src/routes/Home.js
+++ sweeter/src/routes/Home.js
@@ -1,4 +1,40 @@
-import React from "react";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useState } from "react";
+import { dbService } from "../fbBase";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [text, setText] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(dbService, "sweets"), {
+        text,
+        createAt: Date.now(),
+      });
+      setText("");
+    } catch (error) {
+      console.log(`ERROR:`, error);
+    }
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+
+    setText(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={text}
+          type="text"
+          placeholder="what's on your mind?"
+          onChange={onChange}
+        />
+        <input type="submit" value="sweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
