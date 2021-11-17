722ef5ce39b23bc04f2f241022ce1751a46be8b3
shyunju7 <shyunju7@naver.com>
2021-11-17 15:14:35 +0900

    Feat get sweets

--- sweeter/src/routes/Home.js
+++ sweeter/src/routes/Home.js
@@ -1,17 +1,34 @@
-import { addDoc, collection } from "firebase/firestore";
-import React, { useState } from "react";
+import { addDoc, collection, getDoc, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../fbBase";
 
 const Home = () => {
-  const [text, setText] = useState("");
+  const [sweet, setSweet] = useState("");
+  const [sweetList, setSweetList] = useState([]);
+
+  const getTextList = async () => {
+    const querySnapshot = await getDocs(collection(dbService, "sweets"));
+    querySnapshot.forEach((docs) => {
+      const textObject = {
+        ...docs.data(),
+        id: docs.id,
+      };
+      setSweetList((prev) => [...prev, textObject]);
+    });
+  };
+
+  useEffect(() => {
+    getTextList();
+  }, []);
+
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
       await addDoc(collection(dbService, "sweets"), {
-        text,
+        sweet,
         createAt: Date.now(),
       });
-      setText("");
+      setSweet("");
     } catch (error) {
       console.log(`ERROR:`, error);
     }
@@ -21,19 +38,25 @@ const Home = () => {
       target: { value },
     } = e;
 
-    setText(value);
+    setSweet(value);
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input
-          value={text}
+          value={sweet}
           type="text"
           placeholder="what's on your mind?"
           onChange={onChange}
         />
         <input type="submit" value="sweet" />
       </form>
+
+      {sweetList.map((sweet) => (
+        <div key={sweet.id}>
+          <h2>{sweet.text}</h2>
+        </div>
+      ))}
     </div>
   );
 };
