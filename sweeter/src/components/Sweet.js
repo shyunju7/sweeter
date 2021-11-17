0c6fcace0784178c22370c567d1b442536d106c5
shyunju7 <shyunju7@naver.com>
2021-11-17 16:50:43 +0900

    Feat updateSweet

--- sweeter/src/components/Sweet.js
+++ sweeter/src/components/Sweet.js
@@ -1,15 +1,62 @@
-import React from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { dbService } from "../fbBase";
 
 const Sweet = ({ sweetObject, isOwner }) => {
+  const [isEditing, setEditing] = useState(false);
+  const [newSweet, setNewSweet] = useState(sweetObject.text);
+  const deleteSweet = async () => {
+    const ok = window.confirm("Are you sure you want to delete this sweet?");
+
+    if (ok) {
+      await deleteDoc(doc(dbService, "sweets", sweetObject.id));
+    } else {
+      return;
+    }
+  };
+
+  const editSweet = () => {
+    setEditing((prev) => !prev);
+  };
+
+  const onChange = (e) => {
+    setNewSweet(e.target.value);
+  };
+
+  const updateSweet = async () => {
+    await updateDoc(doc(dbService, "sweets", sweetObject.id), {
+      text: newSweet,
+    });
+
+    setEditing((prev) => !prev);
+  };
+
   return (
     <div>
-      <h2>{sweetObject.text}</h2>
-
-      {isOwner && (
+      {isEditing ? (
         <>
-          <button>edit</button>
-          <button>delete</button>
+          <form onSubmit={updateSweet}>
+            <input
+              type="text"
+              value={newSweet}
+              placeholder="edit sweet"
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="update" />
+          </form>
+          <button onClick={() => setEditing((prev) => !prev)}>cancel</button>
         </>
+      ) : (
+        <div>
+          <h2>{sweetObject.text}</h2>
+          {isOwner && (
+            <>
+              <button onClick={editSweet}>edit</button>
+              <button onClick={deleteSweet}>delete</button>
+            </>
+          )}
+        </div>
       )}
     </div>
   );
