4775e5b1c0566ef59188b1c4a4d52fdf4c4234e1
shyunju7 <shyunju7@naver.com>
2021-11-18 14:51:57 +0900

    Fix userName settings

--- sweeter/src/components/App.js
+++ sweeter/src/components/App.js
@@ -1,15 +1,26 @@
 import AppRouter from "./Router";
 import React, { useEffect, useState } from "react";
 import { authService } from "../fbBase";
+import { updateProfile } from "firebase/auth";
 
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObject, setUserObject] = useState(null);
+
+  const updateUserName = async (userName) => {
+    await updateProfile(userObject, { displayName: userName });
+  };
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+
+        if (!user.displayName) {
+          const userName = user.email.substring(0, user.email.indexOf("@"));
+          updateUserName(userName);
+        }
         setUserObject(user);
       } else {
         setIsLoggedIn(false);
