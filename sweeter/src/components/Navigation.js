533b01176132f0857f2475696d10b02786b08f80
shyunju7 <shyunju7@naver.com>
2021-11-18 14:52:16 +0900

    Feat UserObject settings

--- sweeter/src/components/Navigation.js
+++ sweeter/src/components/Navigation.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Navigation = () => (
+const Navigation = ({ userObject }) => (
   <nav>
     <ul>
       <li>
         <Link to="/">HOME</Link>
       </li>
       <li>
-        <Link to="/profile">PROFILE</Link>
+        <Link to="/profile">{userObject.displayName} 's ROFILE</Link>
       </li>
     </ul>
   </nav>
