5caac4f0a7f5d8a485a94f37995ecaf9fc3543f2
shyunju7 <shyunju7@naver.com>
2021-11-19 17:55:12 +0900

    Feat Selected li & Style

--- sweeter/src/components/Navigation.js
+++ sweeter/src/components/Navigation.js
@@ -1,15 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
-const Navigation = ({ userObject }) => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">HOME</Link>
-      </li>
-      <li>
-        <Link to="/profile">My PROFILE</Link>
-      </li>
-    </ul>
-  </nav>
-);
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  list-style: none;
+`;
+
+const Item = styled.li`
+  color: ${(props) => (props.current ? "#04aaff" : "#ffffff")};
+`;
+
+const NavLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 10px;
+  text-decoration: none;
+
+  &:first-child {
+    margin-right: 32px;
+  }
+`;
+
+const Navigation = () => {
+  const location = useLocation();
+
+  return (
+    <nav>
+      <List>
+        <Item current={location.pathname === "/"}>
+          <NavLink to="/">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </NavLink>
+        </Item>
+        <Item current={location.pathname === "/profile"}>
+          <NavLink to="/profile">
+            <FontAwesomeIcon icon={faUser} size="2x" />
+          </NavLink>
+        </Item>
+      </List>
+    </nav>
+  );
+};
+
 export default Navigation;
