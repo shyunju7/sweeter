67970528a0abc4ce4411b235c7c76a136a1dfd45
shyunju7 <shyunju7@naver.com>
2021-11-19 18:25:10 +0900

    Style Profile

--- sweeter/src/routes/Profile.js
+++ sweeter/src/routes/Profile.js
@@ -3,6 +3,29 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService, dbService } from "../fbBase";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 32px;
+`;
+
+const ProfileForm = styled.form`
+  border-bottom: 1px solid rgba(255, 255, 255, 0.9);
+  padding-bottom: 30px;
+  margin-bottom: 12px;
+  display: flex;
+  width: 320px;
+  flex-direction: column;
+`;
+
+const LogoutBtn = styled.button`
+  outline: none;
+  border: none;
+  background-color: tomato;
+  font-size: 14px;
+`;
 
 const Profile = ({ userObject, refreshUser }) => {
   const [newDisplayName, setNewDisplayName] = useState(userObject.displayName);
@@ -47,19 +70,22 @@ const Profile = ({ userObject, refreshUser }) => {
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
+    <Container>
+      <ProfileForm onSubmit={onSubmit}>
         <input
+          className="formInput profile_input"
           type="text"
           placeholder="Display name"
           value={newDisplayName}
           onChange={onChange}
         />
-        <input type="submit" value="Update Profile" />
-      </form>
+        <input type="submit" value="Update Profile" className="formBtn" />
+      </ProfileForm>
 
-      <button onClick={onLogout}>Logout</button>
-    </div>
+      <LogoutBtn onClick={onLogout} className="formBtn">
+        Logout
+      </LogoutBtn>
+    </Container>
   );
 };
 
