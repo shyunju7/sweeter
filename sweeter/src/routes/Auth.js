9900081bf9d235aedd05c6f134adad3c37eb32ee
shyunju7 <shyunju7@naver.com>
2021-11-19 14:37:28 +0900

    Style Auth

--- sweeter/src/routes/Auth.js
+++ sweeter/src/routes/Auth.js
@@ -1,13 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 import AuthForm from "../components/AuthForm";
 import SocialLogin from "../components/SocialLogin";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Auth = () => {
   return (
-    <div>
+    <Container>
       <AuthForm />
       <SocialLogin />
-    </div>
+    </Container>
   );
 };
 
