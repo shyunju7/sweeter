f558c55d4d20ac9c46e1fea7d64c68fe4f54c2b7
shyunju7 <shyunju7@naver.com>
2021-11-19 18:30:13 +0900

    Add Footer

---
+++ sweeter/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.footer`
+  position: absolute;
+  bottom: 0;
+
+  text-align: end;
+  width: 100%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      &copy; {new Date().getFullYear()} Sweeter with Nomad Coders
+    </Container>
+  );
+};
+
+export default Footer;
