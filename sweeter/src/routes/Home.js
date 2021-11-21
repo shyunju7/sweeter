5cb9c24086a8e3172d8614f8b756073241fc0858
shyunju7 <shyunju7@naver.com>
2021-11-21 19:22:42 +0900

    Style apllication

--- sweeter/src/routes/Home.js
+++ sweeter/src/routes/Home.js
@@ -1,21 +1,31 @@
+import { onAuthStateChanged } from "firebase/auth";
 import { collection, onSnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import Sweet from "../components/Sweet";
 import SweetFactory from "../components/SweetFactory";
-import { dbService } from "../fbBase";
+import { authService, dbService } from "../fbBase";
 
 const Home = ({ userObject }) => {
   const [sweetList, setSweetList] = useState([]);
 
   // snapshot - db에 변경을 알림
   useEffect(() => {
-    onSnapshot(collection(dbService, "sweets"), (snapShot) => {
-      const sweetArray = snapShot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
+    const unsubscribe = onSnapshot(
+      collection(dbService, "sweets"),
+      (snapShot) => {
+        const sweetArray = snapShot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
 
-      setSweetList(sweetArray);
+        setSweetList(sweetArray);
+      }
+    );
+
+    onAuthStateChanged(authService, (user) => {
+      if (user == null) {
+        unsubscribe();
+      }
     });
 
     return () => {
