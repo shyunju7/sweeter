9ae2c2e837ca2a120be2f1095d6186a42b9429ce
shyunju7 <shyunju7@naver.com>
2022-02-16 16:58:44 +0900

    Feat set Applying Dark Mode to Footer

--- sweeter/src/components/Footer.js
+++ sweeter/src/components/Footer.js
@@ -6,11 +6,12 @@ const Container = styled.footer`
   bottom: 0;
   text-align: center;
   width: 100%;
+  color: ${(props) => (props.isDarkMode ? "#ffffff" : "#333333")};
 `;
 
-const Footer = () => {
+const Footer = ({ isDarkMode }) => {
   return (
-    <Container>
+    <Container isDarkMode={isDarkMode}>
       &copy; {new Date().getFullYear()} Sweeter with Nomad Coders
     </Container>
   );
